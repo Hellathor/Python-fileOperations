4d93b3cd9f74b984ff53c1bca4881e1a085ab886
Ene-une Reuben Ochedi <rubycodes14@gmail.com>
2020-06-16 02:11:18 +0100

    Update fileOperations.py

--- fileOperations.py
+++ fileOperations.py
@@ -19,7 +19,7 @@ pixels1 = "##" * 15
 pixels2 = "**" * 11
 
 
-designPrompt1 = "Choose from the options below:\n\nCHOICE: (1, 2, 3)\n" + pixels1 + "\n1. Open File\n2. Create \
+designPrompt1 = "Choose from the options below:\n\nCHOICE: (1, 2, 3)\n" + pixels1 + "\n1. Open a File\n2. Create \
 a File\n3. Delete a file\n\tPress Enter to Exit.\n" + pixels1 + "\n"
 
 designPrompt2 = "ACTION: (1, 2, 3, 4)\n" + pixels2 + "\n1. Add_To_File.\n2. Edit_File\n3. View Content\
@@ -28,7 +28,10 @@ designPrompt2 = "ACTION: (1, 2, 3, 4)\n" + pixels2 + "\n1. Add_To_File.\n2. Edit
 
 
 ''' From 'choice' to 'operation' to 'action' to ...
-
+    ALWAYS operation() AFTER choice()
+            OR
+    REMOVE ALL choice() BEFORE operation() AND
+    INCLUDE choice() IN operation()
 '''
 
 
@@ -38,16 +41,16 @@ def quit():
     print("\n", "+-" * 51)
     print("\tEmail: rubycodes14@gmail.com\t  LinkedIn: Ene-une Ochedi\t  GitHub: RubyCodes14\n\tFacebook: Ene-une Reuben Ochedi\t\t\tWhatsApp: +2348136020460")
     print("\n", "==" * 51)
-    time.sleep(5)
+    time.sleep(10)
     exit()
 
 if(os.access("files", os.F_OK)):
     pass
 else:
-    print("FILE NOT FOUND: WE COULD NOT LOCATE A FOLDER NAMED \"files\" IN \
-THE DIRECTORY \n\t\tPLEASE CREATE ONE MANUALLY IN THE SAME PATH AS THIS FILE.")
+    os.mkdir("files")
+    print("A folder named 'files' has been created")
     print("ALL YOUR FILES WILL BE SAVED IN THE FOLDER(files).\n")
-    quit()
+    
 
 
 def up_Or_Low_Case(text):
@@ -59,18 +62,23 @@ def up_Or_Low_Case(text):
 
 def choice():
     print(designPrompt1)
+    global options
     options = input("Enter Choice: ")
     return options
 
 
 
 
-def fileName(flag):
+def fileName(flag = 0):
     global filename
     global filename_    #filename to be displayed, not including the directory___/files
     filename = input("\nEnter desired filename: ")
     if filename == "":
-        quit()
+        if flag <= 2:
+            #choice()
+            operation()
+        else:
+            quit()
     else:
         filename_ = up_Or_Low_Case(filename) + file_Extension
         filename = folderName + up_Or_Low_Case(filename) + file_Extension
@@ -81,7 +89,7 @@ def fileName(flag):
                 return filename
             else:            
                 trials = "No such file exist.\nTry again\
-(Or press enter to go back to menu \[Trials remaining " + str(3 - args) + "])"
+(Or press enter to go back to menu \n[Trials remaining " + str(3 - args) + "])"
                 if args > 2:
                     trials = "No such file exist.\n[Trials remaining \
 " + str(3 - args) + "]"
@@ -133,9 +141,12 @@ def _view():
         view = open(filename, "r")
         output = view.read()
         print("Here is the content of your file: ", filename_)
-        print(output, "\n")
-        view.close()
-        action()
+        if len(output) < 1:
+            print("\t\tEMPTY FILE\n### There's nothing to show ###")
+        else:
+            print(output, "\n")
+            #view.close()
+            action()
     finally:
         view.close()
 
@@ -159,7 +170,8 @@ wish to continue?")
     elif task == view_Content:
         _view()
     elif task == back_To_Main_Menu:
-        choice()
+        #choice()
+        operation()
     else:
         quit()
 
@@ -181,34 +193,38 @@ def file_Status():
         
 
 
-_choice = choice()
+#_choice = choice()
 
 def operation(): #Declare 'filename', 'file_Status' as global
-    if _choice == open_File:
+    choice()
+    if options == open_File:
         print("OPENNING A FILE")
         flag = 0
         fileName(flag)
         print("File ", filename_, " opened")
         action()
 
-    elif _choice == create_File:
+    elif options == create_File:
         print("CREATING A FILE")
         flag = "create"
         fileName(flag)
         file_Status()
         action()
 
-    elif _choice == delete_File:
+    elif options == delete_File:
         print("DELETING A FILE")
         flag = "delete"
         fileName(flag)
         if fileExist():
+            fileName().clear()
             os.remove(filename)
             print("File deleted successfully\n")
-            chioce()
+            #chioce()
+            operation()
         else:
             print("Oops! The file does not even exist.\n")
-            choice()
+            #choice()
+            operation()
             
     else:
         quit()
